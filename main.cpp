095f62a1a5472dca71cf9ac38d820a33257fae57
Efrain Balboa <2430252@upv.edu.mx>
2025-09-22 20:24:10 -0600

    Se agrego lectura de .csv con un argumento

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+int main(int s, char* argv[]) {
+    // Comprueba si se introdujo datos
+    if (s < 2) {
+        std::cerr << "No datos enviados" << std::endl;
+        return 1;
+    }
+
+    if (s > 2) {
+        std::cerr << "Error: no se permite en espacios, intente de nuevo." << std::endl;
+        return 1;
+    }
+
+    string nameArchive = string(argv[1]) + ".csv";
+    std::ifstream archive(nameArchive);
+    // Comprueba que se encuentre un archivo existente
+    if (!archive.is_open()) {
+        cerr << "No se pudo abrir el archivo: " << nameArchive << std::endl;
+        return 1;
+    }
+    cout << "Archivo abierto: " << nameArchive << endl;
     return 0;
 }
